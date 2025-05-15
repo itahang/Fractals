7f3646823fc50ecf0592fe83ac4f67ac34578340
itahang <hangita101@gmail.com>
2025-05-15 22:54:30 +0545

    changing actinos

--- test/test.py
+++ test/test.py
@@ -4,27 +4,29 @@ import ctypes
 import os
 
 # lib_path = ""
-
+lib_path = Path(__file__).parent.parent / "libs" / "mandelbrot.dll"
 # if os.name == "nt":
 #     lib_path = Path(__file__).parent.parent / "libs" / "mandelbrot.dll"
 # elif os.name == "posix":
 #     lib_path = Path(__file__).parent.parent / "libs" / "mandel.so"
 
 
-# def try_loadingLibrary():
-#     try:
-#         cuda_lib = ctypes.CDLL(str(lib_path))
-#     except OSError as e:
-#         pytest.fail(f"Fail to load cuda library")
+def try_loadingLibrary():
+    try:
+        cuda_lib = ctypes.CDLL(str(lib_path))
+    except OSError as e:
+        pytest.fail(f"Fail to load cuda library")
 
-# def test_loading():
-#     try_loadingLibrary()
+def test_loading():
+    try_loadingLibrary()
 
-# def test_function_exist():
-#     cuda_lib = ctypes.CDLL(str(lib_path))
-#     func = getattr(cuda_lib, "mandelbrot_gpu", None)
-#     assert func is not None, "mandelbrot_gpu function is missing"
+def test_function_exist():
+    cuda_lib = ctypes.CDLL(str(lib_path))
+    func = getattr(cuda_lib, "mandelbrot_gpu", None)
+    assert func is not None, "mandelbrot_gpu function is missing"
 
 
+
+## Check if image exists
 
 
