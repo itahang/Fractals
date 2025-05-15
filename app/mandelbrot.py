8dfc8dfdd3939dbe3889030b92be63cd09381bcf
itahang <hangita101@gmail.com>
2025-05-15 22:38:14 +0545

    kind of works

--- app/mandelbrot.py
+++ app/mandelbrot.py
@@ -19,19 +19,24 @@ cuda_lib.mandelbrot_gpu.argtypes = [
     ctypes.POINTER(ctypes.c_uint),
     ctypes.c_uint,
     ctypes.c_uint,
-    ctypes.c_double
+    ctypes.c_double,
+    ctypes.c_double,
+    ctypes.c_double,
+    
 ]
 
 
+# extern "C" EXPORT void mandelbrot_gpu(unsigned int *image, const unsigned int width, const unsigned int height,const double center_x,const double center_y, const double zoom)
 
-def mandelBrotWrapper(width,height,zoom):
+def mandelBrotWrapper(width,height,center_x,center_y,zoom):
     
     image = np.zeros((height, width), dtype=np.uint32)
-
     cuda_lib.mandelbrot_gpu(
         image.ctypes.data_as(ctypes.POINTER(ctypes.c_uint)),
         ctypes.c_uint(width),
         ctypes.c_uint(height),
+        ctypes.c_double(center_x),
+        ctypes.c_double(center_y),
         ctypes.c_double(zoom)
     )
     return image
--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ async def hmande(
     center_x: float = -0.5,
     center_y: float = 0.0
 ):
-    image = mandelBrotWrapper(width, height, zoom)
+    image = mandelBrotWrapper(width, height,center_x,center_y, zoom)
     genImage(image)
 
     with open("static/index.html", "r") as f:
